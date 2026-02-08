f3e204596c43c0476ebe1f3177856894e8d0340e
K N <niedbalaproject@gmail.com>
2026-02-08 22:00:19 +0100

    Change the function name to __to_km

--- app/main.py
+++ app/main.py
@@ -15,14 +15,14 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km:g})"
 
-    def __convert_to_km__(self, other: Distance | Number) -> float:
+    def __to_km__(self, other: Distance | Number) -> float:
         return other.km if isinstance(other, Distance) else float(other)
 
     def __add__(self, other: Distance | Number) -> Distance:
-        return Distance(self.km + self.__convert_to_km__(other))
+        return Distance(self.km + self.__to_km__(other))
 
     def __iadd__(self, other: Distance | Number) -> Distance:
-        self.km += self.__convert_to_km__(other)
+        self.km += self.__to_km__(other)
         return self
 
     def __mul__(self, other: Number) -> Distance:
@@ -32,10 +32,10 @@ class Distance:
         return Distance(round(self.km / float(other), 2))
 
     def __lt__(self, other: Distance | Number) -> bool:
-        return self.km < self.__convert_to_km__(other)
+        return self.km < self.__to_km__(other)
 
     def __gt__(self, other: Distance | Number) -> bool:
-        return self.km > self.__convert_to_km__(other)
+        return self.km > self.__to_km__(other)
 
     def __eq__(self, other: object) -> bool:
         if isinstance(other, Distance):
@@ -45,7 +45,7 @@ class Distance:
         return NotImplemented
 
     def __le__(self, other: Distance | Number) -> bool:
-        return self.km <= self.__convert_to_km__(other)
+        return self.km <= self.__to_km__(other)
 
     def __ge__(self, other: Distance | Number) -> bool:
-        return self.km >= self.__convert_to_km__(other)
+        return self.km >= self.__to_km__(other)
